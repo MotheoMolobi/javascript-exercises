9a9be7d2e77b571069bea0cb79d9a94b117d10cd
MotheoMolobi <molobimotheo@gmail.com>
2024-10-23 12:29:50 +0200

    complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    for(i = 1; i <= num; i++) {
+        console.log(word)
+    }
 
 };
-
+repeatString('hey' , 3);
 // Do not edit below this line
 module.exports = repeatString;
