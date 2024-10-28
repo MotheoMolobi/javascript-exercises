9f92e814b52e58dfd18a43dee2ba667221ef4e6c
MotheoMolobi <molobimotheo@gmail.com>
2024-10-28 08:52:54 +0200

    Complete activity

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+   let ans = false
+
+        if(year % 4 == 0 && (year % 100 !== 0 || year % 400 == 0 )) {
+          ans = true
+        }
+        else {
+          ans = false
+        }
+      
+        return ans
 };
 
 // Do not edit below this line
