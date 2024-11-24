839eda872fbe1a35dbb01dcd39c38a96125080d9
MotheoMolobi <molobimotheo@gmail.com>
2024-11-24 19:15:10 +0200

    complete activity

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let num = 0;
+
+  for(let i = 0; i < a.length; i++){
+    num += a[i];
+  }
+  return num;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let total = 1
+  for(let i = 0; i < a.length; i++){
+    total *= a[i]
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  total = 1;
+  if(a == 0) {
+    return 1
+  }
+  else {
+    for(let i = 1; i <= a; i++){
+      total = total * i
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
---
+++ practice/prac-One/index.js
@@ -0,0 +1,7 @@
+
+document.getElementById("mySubmit").onclick = function(){
+    let username = "Motheo Molobi";
+    document.getElementById("display").textContent = `Hello ${username}`
+};
+
+console.log('hello');
