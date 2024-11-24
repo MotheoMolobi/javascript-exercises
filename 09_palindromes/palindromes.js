7b89371a3de635bd5085938266aa70a47a4484ed
MotheoMolobi <molobimotheo@gmail.com>
2024-11-24 20:48:38 +0200

    complete activity

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-};
+    const clean = a.toLowerCase().split('').filter((character) => alphanumerical.includes(character)).join('');
+    const rev = clean.split('').reverse().join('')
+  
+    return rev === clean;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
