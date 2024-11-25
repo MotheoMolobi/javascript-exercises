e8b512d3c95d65864d4cc96671035b409f9fb5ff
MotheoMolobi <molobimotheo@gmail.com>
2024-11-25 21:10:41 +0200

    complete activity

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    let titles = []
+  
+    for (let i = 0; i < books.length; i++){
+      titles[i] = books[i].title
+    }
+    return titles
+    // Another shorter more optimal method to do this 
+    // return books.map((book) => book.title);
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
